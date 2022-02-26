2efefbbdbb9b79cb5f2fe48f6e754f87fea12b8c
Pedro Henrique <pedro.henrique59@outlook.com>
2022-02-26 15:53:09 -0300

    Criando relacionamento produto categoria.

--- src/main/java/Aplicacao/Main.java
+++ src/main/java/Aplicacao/Main.java
@@ -1,7 +1,9 @@
 package Aplicacao;
 
 import Aplicacao.domain.Categoria;
+import Aplicacao.domain.Produto;
 import Aplicacao.repositories.CategoriaRepository;
+import Aplicacao.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class Main implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -23,6 +28,19 @@ public class Main implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Categoria categoria1 = new Categoria(null, "Informática");
         Categoria categoria2 = new Categoria(null, "Escritório");
+
+        Produto produto1 = new Produto(null, "Computador", 2000.00);
+        Produto produto2 = new Produto(null, "Impressora", 800.00);
+        Produto produto3 = new Produto(null, "Mouse", 80.00);
+
+        categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+        categoria2.getProdutos().add(produto2);
+
+        produto1.getCategorias().add(categoria1);
+        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+        produto3.getCategorias().add(categoria1);
+
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
     }
 }
