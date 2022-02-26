ec53e628fc548504140482334141562ff5313317
Pedro Henrique <pedro.henrique59@outlook.com>
2022-02-26 12:39:16 -0300

    Adicionando configuração H2 e mapeando entidade Categoria.

--- src/main/java/Aplicacao/resources/CategoriaResource.java
+++ src/main/java/Aplicacao/resources/CategoriaResource.java
@@ -1,16 +1,25 @@
 package Aplicacao.resources;
 
+import Aplicacao.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-     public String listar(){
-         return "Tá funcionando";
-     }
+    public List<Categoria> listar() {
 
+        Categoria categoria = new Categoria();
+        Categoria categoria1 = new Categoria();
+
+        List<Categoria> categorias = new ArrayList<>(Arrays.asList(categoria, categoria1));
+        return categorias;
+    }
 }
