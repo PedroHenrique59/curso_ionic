853fd23b6aa8e1339b7579022396c40fc63994da
Pedro Henrique <pedro.henrique59@outlook.com>
2022-02-26 14:48:55 -0300

    Criando operação de insert.

--- src/main/java/Aplicacao/Main.java
+++ src/main/java/Aplicacao/Main.java
@@ -1,11 +1,28 @@
 package Aplicacao;
 
+import Aplicacao.domain.Categoria;
+import Aplicacao.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
     }
 }
