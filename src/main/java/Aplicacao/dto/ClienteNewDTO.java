4770e56916ef6f4a137735c32455d38720ca6ee5
pedro.walter <pedro.walter@basis.com.br>
2022-04-05 15:14:49 -0300

    Anotações de validação.

--- src/main/java/Aplicacao/dto/ClienteNewDTO.java
+++ src/main/java/Aplicacao/dto/ClienteNewDTO.java
@@ -1,20 +1,41 @@
 package Aplicacao.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 public class ClienteNewDTO {
 
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "E-mail inválido")
     private String email;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cpfOuCnpj;
+
     private Integer tipoCliente;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String logradouro;
+
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String telefone1;
+
     private String telefone2;
+
     private String telefone3;
 
     private Integer cidadeId;
