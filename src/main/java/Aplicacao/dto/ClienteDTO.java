d70b42a677bcee6dcc292eca97a704dcbddcaa82
pedro.walter <pedro.walter@basis.com.br>
2022-04-06 13:17:18 -0300

    Validando e-mail existente na atualização de cliente.

--- src/main/java/Aplicacao/dto/ClienteDTO.java
+++ src/main/java/Aplicacao/dto/ClienteDTO.java
@@ -1,13 +1,14 @@
 package Aplicacao.dto;
 
 import Aplicacao.domain.Cliente;
-import org.hibernate.EmptyInterceptor;
+import Aplicacao.services.Validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private Integer id;
